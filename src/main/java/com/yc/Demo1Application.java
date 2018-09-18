090d2566eeab0c66ad31cb475de8bec4ace93cb9
hexiang <1973277886@qq.com>
2018-09-18 13:45:27 +0800

    何翔

--- src/main/java/com/yc/Demo1Application.java
+++ src/main/java/com/yc/Demo1Application.java
@@ -1,11 +1,13 @@
-package com.yc.demo;
+package com.yc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class Demo1Application {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 	}
