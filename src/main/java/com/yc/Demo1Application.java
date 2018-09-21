46ce1e9fe9c249a05db29a3a7138271e1253c3ac
hexiang <1973277886@qq.com>
2018-09-21 16:59:25 +0800

    hexiang

--- src/main/java/com/yc/Demo1Application.java
+++ src/main/java/com/yc/Demo1Application.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @ServletComponentScan
+@EnableSwagger2
 public class Demo1Application {
 	
 	public static void main(String[] args) {
